cfe36190173eefdc3caf2a6092b7c33cb8c6df64
Danilo Azevedo <114174894+danilosazevedo@users.noreply.github.com>
2023-09-11 21:25:38 -0300

    Finalizando Tarefa
    Convertendo variável primitiva para wrapper.

--- Tarefa_Wrapper/src/index/Product.java
+++ Tarefa_Wrapper/src/index/Product.java
@@ -1,12 +1,15 @@
 package index;
 
 public class Product {
-
-	public static void main(String[] args) {
 	
-		Integer code = 10 ;
-		System.out.print(code);
+    public static void main(String[] args) {
+        // Variável primitiva int
+        int primitivo = 42;
 
-	}
+        // Convertendo para Integer (wrapper)
+        Integer wrapper = Integer.valueOf(primitivo);
 
+        // Agora "wrapper" é um Integer
+        System.out.println(wrapper);
+    }
 }
